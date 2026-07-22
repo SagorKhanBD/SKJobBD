228bbe55d803129d44daeb1f2a70ba34c88ac908
Sagor Khan <sagorkhan251996@gmail.com>
2026-07-22 13:36:25 +0600

    Add main JavaScript file for SK Job BD

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+=========================================
+SK Job BD
+Main JavaScript File
+Version: 1.0
+=========================================
+*/
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("✅ SK Job BD Loaded Successfully");
+
+});
