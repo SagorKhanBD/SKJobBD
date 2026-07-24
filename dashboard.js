ad9c130fc97adda90accfa8ba1684f038a065217
Sagor Khan <sagorkhan251996@gmail.com>
2026-07-24 17:02:07 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,69 @@
+import { db } from "./firebase.js";
 
+import {
+    collection,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+// LocalStorage থেকে মোবাইল নম্বর নিন
+const mobile = localStorage.getItem("userMobile");
+
+if (!mobile) {
+    window.location.href = "login.html";
+}
+
+async function loadUser() {
+
+    try {
+
+        const q = query(
+            collection(db, "users"),
+            where("mobile", "==", mobile)
+        );
+
+        const snapshot = await getDocs(q);
+
+        if (snapshot.empty) {
+
+            alert("User Not Found");
+
+            window.location.href = "login.html";
+
+            return;
+        }
+
+        const user = snapshot.docs[0].data();
+
+        document.getElementById("userName").innerText = user.name || "";
+
+        document.getElementById("userMobile").innerText = user.mobile || "";
+
+        document.getElementById("userType").innerText = user.accountType || "";
+
+        document.getElementById("userInstitution").innerText = user.institution || "";
+
+        document.getElementById("userEmail").innerText = user.email || "Not Provided";
+
+        document.getElementById("userStatus").innerText = user.status || "";
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert(error.message);
+
+    }
+
+}
+
+window.logout = function () {
+
+    localStorage.removeItem("userMobile");
+
+    window.location.href = "login.html";
+
+}
+
+loadUser();
