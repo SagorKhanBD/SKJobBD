9c4fd4d4fb03f641bec5fcc7548d03087bcae055
Sagor Khan <sagorkhan251996@gmail.com>
2026-07-24 17:06:54 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,44 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-// Firebase Config
+import { 
+    getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+
+// Firebase Configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBunjHskHSpPQHyyoaf1eMdx2E3ygw8Amw",
-  authDomain: "sk-job-bd.firebaseapp.com",
-  projectId: "sk-job-bd",
-  storageBucket: "sk-job-bd.firebasestorage.app",
-  messagingSenderId: "548971925971",
-  appId: "1:548971925971:web:3526256d6b25456ed01f26"
+
+    apiKey: "AIzaSyBunjHskHSpPQHyyoaf1eMdx2E3ygw8Amw",
+
+    authDomain: "sk-job-bd.firebaseapp.com",
+
+    databaseURL: "https://sk-job-bd-default-rtdb.firebaseio.com",
+
+    projectId: "sk-job-bd",
+
+    storageBucket: "sk-job-bd.firebasestorage.app",
+
+    messagingSenderId: "548971925971",
+
+    appId: "1:548971925971:web:3526256d6b25456ed01f26",
+
+    measurementId: "G-TFG7JG25GG"
+
 };
 
+
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Export Authentication & Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+// Firestore
+
+const db = getFirestore(app);
+
+
+// Export
+
+export { db };
