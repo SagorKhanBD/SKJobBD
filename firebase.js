06ec8e255f60bf894ad52a04ac8c8fdecd80c778
Sagor Khan <sagorkhan251996@gmail.com>
2026-07-25 13:54:15 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,41 +1,24 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-
-// আপনার Firebase Config এখানে বসান
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-    apiKey: "YOUR_API_KEY",
-
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-    projectId: "YOUR_PROJECT_ID",
-
-    storageBucket: "YOUR_PROJECT.appspot.com",
-
-    messagingSenderId: "YOUR_SENDER_ID",
-
-    appId: "YOUR_APP_ID"
-
+  apiKey: "AIzaSyBunjHskHSpPQHyyoaf1eMdx2E3ygw8Amw",
+  authDomain: "sk-job-bd.firebaseapp.com",
+  databaseURL: "https://sk-job-bd-default-rtdb.firebaseio.com",
+  projectId: "sk-job-bd",
+  storageBucket: "sk-job-bd.firebasestorage.app",
+  messagingSenderId: "548971925971",
+  appId: "1:548971925971:web:3526256d6b25456ed01f26",
+  measurementId: "G-TFG7JG25GG"
 };
-
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
-
-// Firestore
-
 const db = getFirestore(app);
 
+const auth = getAuth(app);
 
-// Export
-
-export { db };
+export { db, auth };
