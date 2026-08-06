67daba56dd830529488f575b63a6b8f11826af60
Sagor Khan <sagorkhan251996@gmail.com>
2026-08-06 14:28:24 +0600

    Update login.js

--- employer/login.js
+++ employer/login.js
@@ -204,3 +204,29 @@ Employer Login System
 Professional Version
 Completed
 =========================================*/
+// Show Hide Password
+
+const togglePassword =
+document.getElementById("togglePassword");
+
+const password =
+document.getElementById("password");
+
+
+togglePassword.addEventListener("click",()=>{
+
+    if(password.type === "password"){
+
+        password.type="text";
+
+        togglePassword.innerHTML="🙈";
+
+    }else{
+
+        password.type="password";
+
+        togglePassword.innerHTML="👁️";
+
+    }
+
+});
